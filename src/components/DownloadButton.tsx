4013de8b2da7a87a1bf4dd7c47b5504841d526df
duds0 <eduardofigueiredo1930@gmail.com>
2024-05-05 18:12:32 -0300

    Fix: Download Link

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const DownloadButton = (props: Props) => {
     return (
-        <a className={props.className} href={props.link}>
+        <a className={props.className} href={props.link} target="_blank" rel="noopener noreferrer">
             <Image src={props.imageSrc} alt="DownloadButton" height={24} width={24} />
             <h4 className="text-black font-semibold">Download</h4>
         </a>
